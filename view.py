4c231b513f807d84018b1776990a70f92cc67a04
Alyzsa Tatlonghari <alyzsat@gmail.com>
2019-04-06 19:39:17 -0700

    Created buttons for build mode
    Buttons are not yet clickable!

--- view.py
+++ view.py
@@ -1,10 +1,14 @@
 import pygame
+import animal_attributes as aa
 # import mechanics
 
 START_SCREEN = 0
 BUILD_SCREEN = 1
 GAME_SCREEN = 2
 END_SCREEN = 3
+
+ATTRIBUTES = [aa.SKIN, aa.DIET, aa.MOVE, aa.BREATH]
+ANAMES = ["Skin", "Diet", "Movement", "Breathing"]
 
 white = (255,255,255)
 
@@ -14,6 +18,7 @@ class SurvivalGame:
         pygame.display.set_caption("Survival of the Fittest")
         self._running = True
         self._mode = START_SCREEN
+        self._feature = 0
 
 
     def run(self):
@@ -27,6 +32,7 @@ class SurvivalGame:
             elif self._mode == BUILD_SCREEN:
                 surface = pygame.display.get_surface()
                 surface.fill((0,0,0))
+                self._draw_build_screen();
                 
             else:
                 surface = pygame.display.get_surface()
@@ -42,12 +48,18 @@ class SurvivalGame:
             pygame.display.flip()
 
         pygame.quit()
+        
+        
+    def _handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self._running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                self._handle_click()
 
 
     def _draw_start_screen(self):
         surface = pygame.display.get_surface()
-        pixel_width = surface.get_width()
-        pixel_height = surface.get_height()
 
         font = pygame.font.Font(None, 100)
         start_text = font.render("Start", True, white)
@@ -64,22 +76,41 @@ class SurvivalGame:
         surface.blit(title1, (150,100))
         surface.blit(title2, (230,220))
         surface.blit(title3, (200,340))
-
-
-    def _handle_events(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                self._running = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                self._handle_click()
+        
+        
+    def _draw_build_screen(self):
+        surface = pygame.display.get_surface()
+        font = pygame.font.Font(None, 100)
+        
+        title = font.render(ANAMES[self._feature], True, white)
+        surface.blit(title, (400-title.get_width()/2,100))
+        
+        x_pos = 200
+        y_pos = 150
+        
+        for ft in ATTRIBUTES[self._feature].keys():
+            font = pygame.font.Font(None, 70)
+            y_pos += 85
+            pygame.draw.rect(surface,  pygame.color.Color("#60cadb"), (x_pos,y_pos,400,65))
+            text = font.render(ft.capitalize(), True, white)
+            surface.blit(text,(400-text.get_width()/2,y_pos+10))
+            # create a rectangle **button** and create text 
+            
+        
+        
 
 
     def _handle_click(self):
+        cursor_x, cursor_y = pygame.mouse.get_pos()
+        
         if self._mode == START_SCREEN:
-            cursor_x, cursor_y = pygame.mouse.get_pos()
             if cursor_x >= 200 and cursor_x <= 600 \
             and cursor_y >= 500 and cursor_y <= 600:
                 self._mode = BUILD_SCREEN
+                
+#         elif self._mode == BUILD_SCREEN:
+            
+            
         
 
 
